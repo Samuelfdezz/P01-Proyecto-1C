0cd2dda5160403a733c817ef4b2580e52cd3eb84
Samuelfdezz <samferfer1@alum.us.es>
2022-11-15 17:35:42 +0100

    entrega 2

--- src/test_tumores.py
+++ src/test_tumores.py
@@ -1,23 +1,37 @@
 from tumores import *
 
-
+datos = lee_datos(r"P01-Proyecto-1C\data\tumores.csv")
 
 def test_lee_datos():
-    datos = lee_datos(r".\data\tumores.csv")
+   
     print(len(datos))
     print(datos[:3])
     print(datos[85:])
 
+def test_filtra_por_genero():
+    print(filtra_por_genero(datos))
+
+def test_media_icdo():
+    print(media_icdo(datos))
+
+def test_max_grado():
+    print(max_grado(datos))
+
+def test_ordenar_por_categ():
+    print(ordenar_por_categ(datos))
+
+def test_agrupar_por_nombre():
+    print(agrupar_por_nombre(datos))
 
 
 def main():
-    test_lee_datos()
-
-
-
-
-
-
+    ##test_lee_datos()
+    test_filtra_por_genero()
+    test_media_icdo()
+    test_max_grado()
+    test_ordenar_por_categ()
+    test_agrupar_por_nombre()
+    
 if "__main__" == __name__:
     main()
 
--- src/tumores.py
+++ src/tumores.py
@@ -16,3 +16,32 @@ def lee_datos(fichero):
 
         return result
 
+def filtra_por_genero(tumores):
+    res = []
+    for tumor in tumores:
+        if tumor.male=="TRUE":
+            res.append(tumor)
+    return res
+
+def media_icdo(tumores):
+    res = []
+    for tumor in tumores:
+       res.append(tumor.icdo)
+    return sum(res)/len(res)
+
+def max_grado(tumores):
+    return max(tumores,key=lambda tumor:tumor.grade_int)
+
+def ordenar_por_categ(tumores):
+    return sorted(tumores,key=lambda tumor:tumor.icdo,reverse=True)
+
+def agrupar_por_nombre(tumores):
+    res={}
+    for tumor in tumores:
+        clave=tumor.category_name
+        if clave not in res:
+            res[clave]=[tumor]
+        else:
+            res[clave].append(tumor)
+    return res
+        
